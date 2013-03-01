8cdec974f40a2db8867755b335697741d8e41415
Slotos <slotos@gmail.com>
2013-03-02 00:11:19 +0200

    Example tweak
    Changed reddit to yammer whenever applicable
    Otherwise it's the same

--- examples/login/app.js
+++ examples/login/app.js
@@ -1,10 +1,10 @@
 var express = require('express')
   , passport = require('passport')
   , util = require('util')
-  , YammerStrategy = require('passport-yammer').Strategy;
+  , RedditStrategy = require('passport-reddit').Strategy;
 
-var YAMMER_CONSUMER_KEY = "--insert-yammer-consumer-key-here--"
-var YAMMER_CONSUMER_SECRET = "--insert-yammer-consumer-secret-here--";
+var REDDIT_CONSUMER_KEY = "--insert-reddit-consumer-key-here--"
+var REDDIT_CONSUMER_SECRET = "--insert-reddit-consumer-secret-here--";
 
 
 // Passport session setup.
@@ -12,7 +12,7 @@ var YAMMER_CONSUMER_SECRET = "--insert-yammer-consumer-secret-here--";
 //   serialize users into and deserialize users out of the session.  Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Yammer profile is
+//   have a database of user records, the complete Reddit profile is
 //   serialized and deserialized.
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -23,22 +23,22 @@ passport.deserializeUser(function(obj, done) {
 });
 
 
-// Use the YammerStrategy within Passport.
+// Use the RedditStrategy within Passport.
 //   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and Yammer
+//   credentials (in this case, an accessToken, refreshToken, and Reddit
 //   profile), and invoke a callback with a user object.
-passport.use(new YammerStrategy({
-    clientID: YAMMER_CONSUMER_KEY,
-    clientSecret: YAMMER_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/yammer/callback"
+passport.use(new RedditStrategy({
+    clientID: REDDIT_CONSUMER_KEY,
+    clientSecret: REDDIT_CONSUMER_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/reddit/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
-      
-      // To keep the example simple, the user's Yammer profile is returned to
+
+      // To keep the example simple, the user's Reddit profile is returned to
       // represent the logged-in user.  In a typical application, you would want
-      // to associate the Yammer account with a user record in your database,
+      // to associate the Reddit account with a user record in your database,
       // and return that user instead.
       return done(null, profile);
     });
@@ -80,25 +80,25 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-// GET /auth/yammer
+// GET /auth/reddit
 //   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Yammer authentication will involve
-//   redirecting the user to yammer.com.  After authorization, Yammer
-//   will redirect the user back to this application at /auth/yammer/callback
-app.get('/auth/yammer',
-  passport.authenticate('yammer'),
+//   request.  The first step in Reddit authentication will involve
+//   redirecting the user to reddit.com.  After authorization, Reddit
+//   will redirect the user back to this application at /auth/reddit/callback
+app.get('/auth/reddit',
+  passport.authenticate('reddit'),
   function(req, res){
-    // The request will be redirected to Yammer for authentication, so this
+    // The request will be redirected to Reddit for authentication, so this
     // function will not be called.
   });
 
-// GET /auth/yammer/callback
+// GET /auth/reddit/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/yammer/callback', 
-  passport.authenticate('yammer', { failureRedirect: '/login' }),
+app.get('/auth/reddit/callback',
+  passport.authenticate('reddit', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
